208c60b2d15375b38cc7f3d8ee8d907b58598ff2
gbalite <gbalite@uw.edu>
2024-11-13 12:18:37 -0800

    added js file and template

---
+++ static/script.js
@@ -0,0 +1,48 @@
+/**
+ *
+ * Gwyneth Balite
+ * 11/13
+ *
+ */
+
+"use strict";
+
+(function() {
+
+  window.addEventListener("load", init);
+
+  /**
+   * this function adds functionality to the form after the page is loaded
+   */
+  function init() {
+
+      /**
+       * add event listeners to buttons:
+       *    [] timer feature
+       *        [] add 5 minutes
+       *        [] add 10 minutes
+       *        [] start, stop, and reset
+       *    [] to every 'task' div, add a 'task' event listener
+       *    [] event listener for the inserting link form
+       *        [] saving link to local storage
+       */
+
+  }
+
+    /**
+     * TIMER FEATURE
+     *
+     *  add seconds global variable
+     *
+     *  update time field in HTML
+     *
+     */
+
+    /**
+     * TO-DO feature
+     *
+     *
+     *
+     */
+
+})();
