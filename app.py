2adc5ad36c3caad589913ce6687e57c4814ebce0
gbalite <gbalite@uw.edu>
2024-11-14 14:44:37 -0800

    added pseudo code

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+#make /get colors  root that takes in a video id
+    # use the video id to get the video's thumbnail URL
+    # use either urlib or requests to read the binary data of the image
+    # use pillow (a pil) to open the image
+    # use colorthief to get the dominant color
+    # get color scheme from that function
+    # return color scheme
--- static/script.js
+++ static/script.js
@@ -7,42 +7,48 @@
 
 "use strict";
 
-(function() {
+/**
+ * TIMER FEATURE
+ *
+ *  add seconds global variable
+ *
+ *  update time field in HTML & write functions for event listeners
+ *    [] add 5 minutes
+ *    [] add 10 minutes
+ *    [] start, stop, and reset
+ *
+ */
 
-  window.addEventListener("load", init);
+/**
+ * TO-DO FEATURE
+ *
+ * write event listener code
+ *  [] for checkboxes => checking off things => strike through
+ *  [] add 'delete button' when hovering over a task
+ *    [] when clicking delete button, delete the task
+ *  [] task things
+ *    [] when empty teask, if backspacing, go back to previous row and be typing there
+ *    [] when filled task & enter, make new task item
+ *
+ */
 
-  /**
-   * this function adds functionality to the form after the page is loaded
-   */
-  function init() {
-
-      /**
-       * add event listeners to buttons:
-       *    [] timer feature
-       *        [] add 5 minutes
-       *        [] add 10 minutes
-       *        [] start, stop, and reset
-       *    [] to every 'task' div, add a 'task' event listener
-       *    [] event listener for the inserting link form
-       *        [] saving link to local storage
-       */
-
-  }
-
-    /**
-     * TIMER FEATURE
-     *
-     *  add seconds global variable
-     *
-     *  update time field in HTML
-     *
-     */
-
-    /**
-     * TO-DO feature
-     *
-     *
-     *
-     */
-
-})();
+/**
+ * INSERT PLAYLIST FEATURE
+ *   [] add input for inserting url
+ *   [] initialize player
+ *   [] create 'load' function that plays from specific url
+ *   [] when pressing play button, do the following
+ *    - get the playlist ID from the url
+ *    - replace temp image with 'player'
+ *    = call load function
+ *    = potentially save previously used playlists in local storage and be able to play it again
+ *
+ * CHANGE BACKGROUND FEATURE
+ *  [] create an 'on player state change' function
+ *    = if the same video is playing, do nothign
+ *    = else, get video id
+ *    = w/ video id, send a fetch request to flask
+ *    = should return 3 colors,
+ *    = change colors of the screen
+ *
+ */
